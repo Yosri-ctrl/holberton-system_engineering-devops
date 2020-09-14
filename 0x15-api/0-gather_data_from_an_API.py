67c03f66c319beae01e73202fbbe55ffdb7e0367
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-14 22:00:57 +0100

    fix the condition

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,26 @@ from requests import get
 
 
 if __name__ == "__main__":
-    id = argv[1]
-    nbr_tasks = 0
-    tasks_completed = []
+    try:
+        id = argv[1]
+    
+        nbr_tasks = 0
+        tasks_completed = []
 
-    name = get("https://jsonplaceholder.typicode.com/users/{}".format(id))
-    .json().get("name")
-    tasks = get("http://jsonplaceholder.typicode.com/todos").json()
+        name = get("https://jsonplaceholder.typicode.com/users/{}".
+        format(id)).json().get("name")
+        tasks = get("http://jsonplaceholder.typicode.com/todos").json()
 
-    for task in tasks:
-        if (task.get("userId") == int(id)):
-            if str(task.get("completed")) == "True":
-                tasks_completed.append(task)
-            nbr_tasks += 1
+        for task in tasks:
+            if (task.get("userId") == int(id)):
+                if str(task.get("completed")) == "True":
+                    tasks_completed.append(task)
+                nbr_tasks += 1
 
-    print("{} is done with tasks({}/{}):".format(name,
-                                                 len(tasks_completed),
-                                                 nbr_tasks))
-    for i in tasks_completed:
-        print("\t {}".format(i.get("title")))
+        print("{} is done with tasks({}/{}):".format(name,
+                                                    len(tasks_completed),
+                                                    nbr_tasks))
+        for i in tasks_completed:
+            print("\t {}".format(i.get("title")))
+    except:
+        pass
