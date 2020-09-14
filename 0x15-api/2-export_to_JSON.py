e86373500d38259971ed8cc3ab4899255c11f8cc
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-14 22:35:51 +0100

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format
+"""
+import json
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    id = argv[1]
+    list = []
+    name = get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(id)).json().get("username")
+    tasks = get("http://jsonplaceholder.typicode.com/todos").json()
+
+    for task in tasks:
+        if (task.get("userId") == int(id)):
+            temp = {}
+            temp["task"] = task.get("title")
+            temp["completed"] = task.get("completed")
+            temp["username"] = name
+            list.append(temp)
+        
+    with open("{}.json".format(id), 'w+') as jsonfile:
+        json.dump({id: list}, jsonfile)
