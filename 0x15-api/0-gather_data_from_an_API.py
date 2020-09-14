9969c83a2ef7161d6551c17be6698c83d78dfd58
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-14 21:54:23 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+returns information about his/her TODO list progress
+"""
+from sys import argv
+from requests import get
+
+
+if __name__ == "__main__":
+    id = argv[1]
+    nbr_tasks = 0
+    tasks_completed = []
+
+    name = get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(id)).json().get("name")
+    tasks = get("http://jsonplaceholder.typicode.com/todos").json()
+
+    for task in tasks:
+        if (task.get("userId") == int(id)):
+            if str(task.get("completed")) == "True":
+                tasks_completed.append(task)
+            nbr_tasks += 1
+
+    print("{} is done with tasks({}/{}):".format(name,
+                                                 len(tasks_completed), nbr_tasks))
+    for i in tasks_completed:
+        print("\t {}".format(i.get("title")))
