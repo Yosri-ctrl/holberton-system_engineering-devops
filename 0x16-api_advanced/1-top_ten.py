ce6f67869f29f05e4527473caa5f4453d766dd41
Yosri-ctrl <yosribouabid@gmail.com>
2020-09-17 19:20:46 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+""" return the top ten posts """
 import requests
 
 
 def top_ten(subreddit):
-    """ GET subreddit top 10 hot posts """
+    """ return the most hot posts """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'user-agent': 'Yureickane'}
-    r = requests.get(url, headers=headers)
-    if (r.status_code is 404):
+    head = {'user-agent': 'Yureickane'}
+    req = requests.get(url, headers=head)
+    if (req.status_code is 302 or req.status_code is 404):
         print("None")
-    elif 'data' not in r.json():
+    elif 'data' not in req.json():
         print("None")
     else:
-        r = r.json()
-        for post in r['data']['children']:
+        req = req.json()
+        for post in req['data']['children']:
             print(post['data']['title'])
